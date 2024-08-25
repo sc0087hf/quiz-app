31ca57d26573fd4ced8873a31b22c2b0cc1ea350
yajima <t.yajima@itreat.co.jp>
2024-08-25 21:46:03 +0900

    安全ディレクトリの設定後、初回コミット

---
+++ app/Models/Quiz.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Category;
+use App\Models\Option;
+
+class Quiz extends Model
+{
+    use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function options()
+    {
+        return $this->hasMany(Option::class);
+    }
+}
